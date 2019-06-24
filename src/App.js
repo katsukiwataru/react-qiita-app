5f39f7aad7099559dd6b195412eacee46aceb55a
katsukiwataru <a17dc050@dhw.ac.jp>
2019-06-24 19:07:04 +0900

    add qiita api respons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Button from './components/Button'
 
+const server = 'https://qiita.com/api/v2/schema';
+
 class App extends Component {
-  handleClick(event) {
-    console.log('handleClick is called');
+  constructor() {
+    super();
+    this.state = {
+      status: false,
+      result: ''
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  async handleClick(event) {
+    const res = await axios.get(server).catch((err) => {
+      this.setState({
+        status: true,
+        result: err
+      })
+    })
+    console.log(res);
+    this.setState({
+      state: true,
+      result: res.data
+    });
+    console.log(this.state);
+
   }
 
   render() {
     return (
       <div>
         <button onClick={this.handleClick}>Get Data</button>
-        <Button handleAdd={this.handleClick}/>
+        <Button handleAdd={this.handleClick}>Get Data</Button>
       </div>
     );
   }
