fa7501c9284960b46048a49f505576f36761c592
Wataru Katsuki <a17dc050@dhw.ac.jp>
2019-06-25 22:37:22 +0900

    change to methods

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 
-// const Button = (props) => (
-//   <div>
-//     <button onClick={props.handleAdd}>Get Data</button>
-//   </div>
-// );
+const Button = (props) => (
+  <div>
+    <button onClick={props.handleAdd}>Get Data</button>
+  </div>
+);
 
-// export default Button;
+export default Button;
 
-class Button extends Component {
-  render() {
-    console.log(this.props.test);
-    return (
-      <div>
-        <button onClick={this.props.handleAdd}>Get Data</button>
-      </div>
-    )
-  }
-}
+// class Button extends Component {
+//   render() {
+//     console.log(this.props.test);
+//     return (
+//       <div>
+//         <button onClick={this.props.handleAdd}>Get Data</button>
+//       </div>
+//     )
+//   }
+// }
 
 export default Button;
